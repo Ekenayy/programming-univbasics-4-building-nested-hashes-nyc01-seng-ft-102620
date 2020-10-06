0c7af8d3d7799c62447724adb9701609b6125cd3
Ekene N <ekenayy@gmail.com>
2020-10-06 19:11:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   big_hash = {:railroads => 
-  {:pieces => 4, :rent_in_dollars => {}, {:names} => {}} 
+  {:pieces => 4, :rent_in_dollars => {}, :names => {}} 
   }
 
 big_hash
